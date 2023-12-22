6a080771c464663a8ec8784443c1c32ae89903ec
Mahabubur Rahman <mahabubmon@gmail.com>
2023-12-22 13:04:01 +0600

    functionality_adding

--- bst.js
+++ bst.js
@@ -385,3 +385,73 @@ const hdrsQuestions = [
     ],
   },
 ];
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const submitBtn = document.getElementById("submit");
+const results = [];
+
+let currentQuiz = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswers();
+  const currentQuizData = hdrsQuestions[currentQuiz];
+  questionEl.innerText = currentQuizData.title;
+
+  // Display options dynamically
+  currentQuizData.options.forEach((option, index) => {
+    const optionElement = document.getElementById(option.value);
+    optionElement.nextElementSibling.innerText = option.label;
+  });
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
+}
+
+function getSelected() {
+  let answer;
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+  return answer;
+}
+
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+  if (answer) {
+    const result = {
+      question: hdrsQuestions[currentQuiz].title,
+      selectedAnswer: answer, // Store the selected answer instead of correct answer
+    };
+    results.push(result);
+
+    currentQuiz++;
+    if (currentQuiz < hdrsQuestions.length) {
+      loadQuiz();
+    } else {
+      displayResults();
+    }
+  }
+});
+
+function displayResults() {
+  console.log("All results:", results);
+
+  // Display the results as you wish (e.g., show on the page)
+  quiz.innerHTML = `
+    <h2>Results:</h2>
+    <ul>
+      ${results
+        .map(
+          (result) => `<li>${result.question}: ${result.selectedAnswer}</li>`
+        )
+        .join("")}
+    </ul>
+    <button onclick="location.reload()">Reload</button>
+  `;
+}
